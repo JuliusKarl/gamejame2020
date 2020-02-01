be13738f2f3adbc2496cb3e298f5a2cd78ee7cea
Julius Karl <julius.karl.macrohon@gmail.com>
2020-02-01 19:01:41 +1300

    setup

--- src/field/Field.js
+++ src/field/Field.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './field.css'
+import './Field.css'
 
 class Field extends Component {
     render() {
         return (
-            <div>
-                hello
+            <div className="fixed-field">
+                <div className="playing-field">
+                    <div id="tower1"><img src='../assets/0.png'/></div>
+                </div>
             </div>
         );
     }
