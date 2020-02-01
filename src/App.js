4664d5470ef363c48cf3cc376d84e42776d6fe51
Julius Karl <julius.karl.macrohon@gmail.com>
2020-02-01 18:00:58 +1300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Field from './field/Field';
+
+function App() {
+  return (
+    <div className="App">
+      <Field></Field>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/field/Field.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './field.css'
+
+class Field extends Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+    }
+}
+
+export default Field;
