4c50afc91ac6cb06b60254da32c1c3618195ced1
Julius Karl <julius.karl.macrohon@gmail.com>
2020-02-01 19:17:39 +1300

    visible towers

--- src/field/Field.js
+++ src/field/Field.js
@@ -6,7 +6,10 @@ class Field extends Component {
         return (
             <div className="fixed-field">
                 <div className="playing-field">
-                    <div id="tower1"><img src='../assets/0.png'/></div>
+                    <div id="tower1"><img class="tower-img" src={require('../assets/28.png')}/></div>
+                    <div id="tower1"><img class="tower-img" src={require('../assets/0.png')}/></div>
+                    <div id="tower1"><img class="tower-img" src={require('../assets/22.png')}/></div>
+                    <div id="tower1"><img class="tower-img" src={require('../assets/23.png')}/></div>
                 </div>
             </div>
         );
