606c60550a01b1ee35fcc3332273e85f7ca077c4
Cole <colemag383@gmail.com>
2020-02-01 20:57:53 +1300

    Add sample gameState exports for front end dev

---
+++ src/gameState/gameState.js
@@ -0,0 +1,42 @@
+import { cardType } from '../../config/cardType';
+
+let displayCallback;
+
+function setCardPlayedDisplayCallback(f) {
+  // f should be a function that expects one parameter, which is the card played, e.g.:
+  // {targetType: targetType.NONE, imageUrl: '../cards/SET_ALL_TO_LOWEST.jpg'}
+  this.callback = f;
+}
+
+function cardPlayedCallback(e) {
+  this.callback(e);
+}
+
+const state = {
+  abc123: {
+    tower: {
+      health: 15,
+    },
+    hand: [cardType.TRADE_TOWER,
+      cardType.SET_ALL_TO_LOWEST,
+      cardType.UNDO_LAST,
+      cardType.UNDO_LAST,
+      cardType.FOUR],
+  },
+
+  def654: {
+    tower: {
+      health: 29,
+    },
+    hand: [cardType.RESET_TO_15,
+      cardType.FIVE,
+      cardType.ONE_PLUS_ROUND_COUNT,
+      cardType.ONE,
+      cardType.UNDO_LAST],
+  },
+
+};
+
+export {
+  state, setCardPlayedDisplayCallback,
+};
