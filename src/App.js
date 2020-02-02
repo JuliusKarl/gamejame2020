c8fd246b935c5d490e0d334b9cfc668162908502
Gillian <gillian.ng@auckland.ac.nz>
2020-02-02 13:55:51 +1300

    winner callback added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Field from './field/Field';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    // initialize
-  }
-
-  componentDidMount() {
-    const script = document.createElement("script");
-    script.src = "https://www.airconsole.com/api/airconsole-1.7.0.js";
-    script.async = true;
-    this.div.appendChild(script);
-  }
-
   render() {
     return (
-      <div className="App" ref={el => (this.div = el)}>
+      <div className="App">
         <div className="background">
         </div>
         <Field className="playing-field"></Field>
       </div>
     );
   }
+
+  winnerCallback(isHumanWinner) {
+    if(isHumanWinner) {
+      alert("Well done, you won!");
+    } else {
+      alert("You lost, better luck next time!");
+    }
+  }
+  
 }
 
 export default App;
