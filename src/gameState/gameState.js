a567d5d597cf368ed16bf7e2c7e544231722770f
Cole <colemag383@gmail.com>
2020-02-02 14:43:41 +1300

    More stuff, but modules still broken

--- src/gameState/gameState.js
+++ src/gameState/gameState.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-return-assign */
-import { cardType } from '../../config/cardType';
-
+import { cardType } from '../config/cardType';
 let cardPlayedCallback;
 let winnerCallback;
 
@@ -11,27 +10,27 @@ const state = {
     tower: {
       health: 15,
     },
-    hand: Array(6).map(() => drawCard()),
+    hand: [drawCard(), drawCard(), drawCard(), drawCard(), drawCard(), drawCard()],
     isHuman: true
   },
   ai1: {
     tower: {
       health: 15,
     },
-    hand: Array(6).map(() => drawCard()),
+    hand: [drawCard(), drawCard(), drawCard(), drawCard(), drawCard(), drawCard()],
     isHuman: false
   },
   ai2: {
     tower: {
       health: 15,
     },
-    hand: Array(6).map(() => drawCard()),
+    hand: [drawCard(), drawCard(), drawCard(), drawCard(), drawCard(), drawCard()],
     isHuman: false
   }, ai3: {
     tower: {
       health: 15,
     },
-    hand: Array(6).map(() => drawCard()),
+    hand: [drawCard(), drawCard(), drawCard(), drawCard(), drawCard(), drawCard()],
     isHuman: false
   }
 };
@@ -54,39 +53,114 @@ function shuffle(a) {
 }
 
 function checkWinner() {
-  return Object.values(state).filter(player => player.tower.health >= 30);
+  const winner = Object.values(state).filter(player => player.tower.health >= 30)
+  if (winner.length > 0 && winner[0].isHuman) {
+    winnerCallback(true)
+  }
+  else if (winner.length > 0 && !winner[0].isHuman) {
+    winnerCallback(false)
+  }
 }
 
-function cardEffect(card, target) {
+function cardEffect(playerId, card, target) {
   const functions = {
     RANDOMISE_TOWERS: () => {
-      const shuffledTowers = state.map(i => i.tower);
+      const shuffledTowers = Object.entries(state).map(i => i.tower);
       shuffle(shuffledTowers);
       Object.values(state).map((player, index) => player.tower = shuffledTowers[index]);
       return 0;
+    },
+    RESET_TO_15: () => {
+      Object.values(state).forEach(i => i.tower.health = 15)
     },
     SET_ALL_TO_LOWEST: () => {
       const lowestTower = Math.min(...Object.values(state).map((i) => i.tower.health));
       Object.values(state).map(i => i.tower.health = lowestTower);
     },
     TRADE_TOWER: () => {
-      [state[currentPlayer].tower, state[target].tower] = [state[target].tower,
-      state[currentPlayer].tower];
+      [state[playerId].tower, state[target].tower] = [state[target].tower,
+      state[playerId].tower];
     },
     UNDO_LAST: () => {
-      const { targetPlayerId, diff } = log[log.length];
-      state[targetPlayerId].tower.health = -1 * diff;
+      const { target, diff } = log[log.length];
+      state[target].tower.health = -1 * diff;
     },
-
+    ONE_PLUS_ROUND_COUNT: () => {
+      const diff = log.length % Object.entries(state).length
+      if (target === playerId) {
+        state[target].tower.health += diff;
+        return diff
+      } else {
+        state[target].tower.health -= diff;
+        return -1 * diff
+      }
+    },
+    ONE_OR_SEVEN: () => {
+      const sum_diff = log.slice(log.length - 4, log.length).reduce((acc, i) => acc + i.diff)
+      if (sum_diff >= 7) {
+        if (target === playerId) {
+          state[target].tower.health += 7;
+          return 7
+        } else {
+          state[target].tower.health -= 7;
+          return -7
+        }
+      } else {
+        if (target === playerId) {
+          state[target].tower.health += 1;
+          return 1
+        } else {
+          state[target].tower.health -= 1;
+          return -1
+        }
+      }
+    },
+    ONE: () => {
+      if (target === playerId) {
+        state[target].tower.health += 1;
+        return 1
+      } else {
+        state[target].tower.health -= 1;
+        return -1
+      }
+    },
+    TWO: () => {
+      if (target === playerId) {
+        state[target].tower.health += 2;
+        return 2
+      } else {
+        state[target].tower.health -= 2;
+        return -2
+      }
+    },
+    THREE: () => {
+      if (target === playerId) {
+        state[target].tower.health += 3;
+        return 3
+      } else {
+        state[target].tower.health -= 3;
+        return -3
+      }
+    }, FOUR: () => {
+      if (target === playerId) {
+        state[target].tower.health += 4;
+        return 4
+      } else {
+        state[target].tower.health -= 4;
+        return -4
+      }
+    },
   };
 
   const diff = functions[card]();
-  logEvent(e.currentPlayer, e.target, e.card, diff);
+  logEvent(playerId, target, card, diff);
 
   checkWinner();
 }
 
 function aiCard(aiList) {
+  // eslint-disable no-undef
+  const cardType = _config_cardType__WEBPACK_IMPORTED_MODULE_0__.cardType
   const [current, ...tail] = aiList;
   let pickCard = current[1].hand[Math.floor(Math.random() * 6)];
 
@@ -107,21 +181,21 @@ function aiCard(aiList) {
       targetPlayerId = Object.keys(state).filter(i => i !== current[0])[Math.floor(Math.random() * Object.keys(state).length - 1)]
   }
   if (tail.length === 0) {
-    cardEffect(pickCard, targetPlayerId);
+    cardEffect(current[0], pickCard, targetPlayerId);
     cardPlayedCallback(cardType[pickCard])
   } else {
     setTimeout(() => {
       cardPlayedCallback(cardType[pickCard]);
-      cardEffect(pickCard, targetPlayerId);
+      cardEffect(current[0], pickCard, targetPlayerId);
       aiCard(tail);
     }, 1000)
   }
 }
 
 //exported functions
-function playCard(e) {
+function playCard(card, target) {
   //player action
-  cardEffect(e.card);
+  cardEffect("human", card, target);
 
   //ai actions
   aiCard(Object.entries(state).filter(i => !i.isHuman));
@@ -129,12 +203,22 @@ function playCard(e) {
 
 function drawCard() {
   const typeArray = Object.entries(cardType);
-  return Object.fromEntries(typeArray[Math.floor(Math.random * typeArray.length)]);
+  console.log(typeArray);
+  console.log(typeArray[Math.floor(Math.random() * typeArray.length)]);
+  return typeArray[Math.floor(Math.random() * typeArray.length)][0];
+}
+
+function setCardPlayedCallback(f) {
+  cardPlayedCallback = f
+}
+
+function setWinnerCallback(f) {
+  winnerCallback = f
 }
 
 export {
   state,
-  cardPlayedCallback,
-  winnerCallback,
+  setCardPlayedCallback,
+  setWinnerCallback,
   playCard,
 };
